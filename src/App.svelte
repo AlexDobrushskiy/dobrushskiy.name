2a91909d278530ef852e79ca4b6b9982b2a5c3b7
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-03-29 17:05:13 +0300

    Initial commit. Svetle + add img loader + add scss ability with preprocessor.

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Header from './Header.svelte';
+</script>
+
+
+<style>
+
+</style>
+
+<Header/>
---
+++ src/Header.svelte
@@ -0,0 +1,28 @@
+<script>
+  import Logo from '../assets/logo.svg';
+</script>
+
+<style lang="scss">
+  .header {
+    display: flex;
+    flex-direction: row;
+  }
+  .header-menu {
+    ul {
+      list-style-type: none;
+    }
+  }
+</style>
+
+<div class="header">
+    <div class="logo">
+      <img src={Logo} alt="">
+    </div>
+    <div class="header-menu">
+      <ul>
+      <li>
+
+</li>
+</ul>
+    </div>
+</div>
