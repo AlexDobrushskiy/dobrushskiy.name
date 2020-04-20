9fb26d9d2460cd85f06fe3a666f0ca3c93f31e30
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-04-20 16:18:32 +0300

    Carousel

--- src/Recommendation.svelte
+++ src/Recommendation.svelte
@@ -1,16 +1,45 @@
 <script>
   import Photo from '../assets/photo.jpg';
   import BgImage from '../assets/bg1.jpg';
+  import Icon from 'fa-svelte';
+  import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
+  import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
+  import Carousel from '@beyonk/svelte-carousel';
+  import Slide from './Recommendations/Slide.svelte';
+
+  const reviewsData = [
+    {
+      photo: Photo,
+      reviewContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut mattis eros, a sagittis risus. Nullam\n' +
+        '      feugiat, risus eu vehicula dictum, enim arcu posuere sapien, in sodales enim neque eget elit. Nam fermentum eu\n' +
+        '      metus id vehicula. Aliquam eleifend augue a semper lacinia. Maecenas eleifend lacus magna, a dignissim nisi\n' +
+        '      laoreet sollicitudin. Quisque congue eu lectus tincidunt dignissim. Nunc at dolor eros. Pellentesque consequat\n' +
+        '      mauris ac mi volutpat egestas. Nulla facilisi.',
+      author: 'Some Author',
+      authorPosition: 'DIRECTOR OF ENGINEERING AT DUNNHUMBY1'
+    },
+    {
+      photo: Photo,
+      reviewContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut mattis eros, a sagittis risus. Nullam\n' +
+        '      feugiat, risus eu vehicula dictum, enim arcu posuere sapien, in sodales enim neque eget elit. Nam fermentum eu\n' +
+        '      metus id vehicula. Aliquam eleifend augue a semper lacinia. Maecenas eleifend lacus magna, a dignissim nisi\n' +
+        '      laoreet sollicitudin. Quisque congue eu lectus tincidunt dignissim. Nunc at dolor eros. Pellentesque consequat\n' +
+        '      mauris ac mi volutpat egestas. Nulla facilisi.',
+      author: 'Some Other Author',
+      authorPosition: 'DIRECTOR OF ENGINEERING AT Some Other'
+    }
+
+  ];
 </script>
 <style lang="scss">
 
-  $image-width: 300px;
   .main {
     min-height: 777px;
     background-position: 68px;
     background-repeat: no-repeat;
     background-size: calc(100% - 50px);
   }
+
   h1 {
     margin-left: 68px;
     margin-top: 20px;
@@ -19,64 +48,53 @@
     margin-bottom: 0;
   }
 
-  .content {
-    display: flex;
 
-    .image-wrapper {
-      margin-top: 50px;
-      margin-left: 68px;
-      /*position: relative;*/
-      width: $image-width;
-      height: $image-width;
+  /*.arrows {*/
+  /*  display: flex;*/
+  /*  margin-top: 20px;*/
 
-      img {
-        border-radius: 100px;
-        width: $image-width;
-        height: $image-width;
-      }
-    }
-
-    .image-frame {
-      margin-top: 33px;
-      margin-left: calc(17px - #{$image-width});
-      border-radius: 100px;
-      min-width: $image-width;
-      width: $image-width;
-      height: $image-width;
-      border: solid 1px var(--light-navy);
-    }
-    .review {
-      margin-top: 136px;
-      margin-left: 56px;
-      max-width: 530px;
-      p {
-        font-size: 18px;
-        font-weight: 300;
-        color: var(--black);
-      }
-      h5 {
-        margin-top: 40px;
-        margin-bottom: 14px;
-        color: var(--dark-grey);
-        font-size: 24px;
-        font-weight: bold;
-      }
-    }
+  .circle-left, .circle-right {
+    width: 49px;
+    height: 49px;
+    border-radius: 50%;
+    cursor: pointer;
+    color: rgba(255, 255, 255, 0.5);
+    font-size: 25px;
+    line-height: 55px;
+    text-align: center;
   }
+
+  .circle-left {
+    background-color: var(--greyish);
+    margin-right: 15px;
+  }
+
+  .circle-right {
+    background-color: var(--light-navy);
+  }
+
+  /*}*/
 
 
 </style>
 <div class="main" style="background-image: url({BgImage})" id="id-recommendation">
   <h1>Recommendations</h1>
-  <div class="content">
-    <div class="image-wrapper">
-      <img src={Photo} alt="photo">
-    </div>
-    <div class="image-frame"></div>
-    <div class="review">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ut mattis eros, a sagittis risus. Nullam feugiat, risus eu vehicula dictum, enim arcu posuere sapien, in sodales enim neque eget elit. Nam fermentum eu metus id vehicula. Aliquam eleifend augue a semper lacinia. Maecenas eleifend lacus magna, a dignissim nisi laoreet sollicitudin. Quisque congue eu lectus tincidunt dignissim. Nunc at dolor eros. Pellentesque consequat mauris ac mi volutpat egestas. Nulla facilisi.</p>
-      <h5>Some Author</h5>
-      <p>DIRECTOR OF ENGINEERING AT DUNNHUMBY</p>
-    </div>
-  </div>
+  <Carousel perPage={1} duration={1000}>
+      {#each reviewsData as reviewData}
+        <div class="slide-content">
+          <Slide reviewData={reviewData}/>
+        </div>
+      {/each}
+    <span slot="left-control">
+      <div class="circle-left">
+        <Icon icon={faArrowLeft}/>
+      </div>
+    </span>
+    <span slot="right-control">
+      <div class="circle-right">
+        <Icon icon={faArrowRight}/>
+      </div>
+    </span>
+
+  </Carousel>
 </div>
---
+++ src/Recommendations/Slide.svelte
@@ -0,0 +1,73 @@
+<script>
+  export let reviewData;
+
+</script>
+<style lang="scss">
+  $image-width: 300px;
+  
+  .content {
+    display: flex;
+
+  .image-wrapper {
+    margin-top: 50px;
+    margin-left: 68px;
+    /*position: relative;*/
+    width: $image-width;
+    height: $image-width;
+
+  img {
+    border-radius: 100px;
+    width: $image-width;
+    height: $image-width;
+  }
+
+  }
+
+  .image-frame {
+    margin-top: 33px;
+    margin-left: calc(17px - #{$image-width});
+    border-radius: 100px;
+    min-width: $image-width;
+    width: $image-width;
+    height: $image-width;
+    border: solid 1px var(--light-navy);
+  }
+
+  .review {
+    margin-top: 136px;
+    margin-left: 56px;
+    max-width: 530px;
+
+  p {
+    font-size: 18px;
+    font-weight: 300;
+    color: var(--black);
+  }
+
+  h5 {
+    margin-top: 40px;
+    margin-bottom: 14px;
+    color: var(--dark-grey);
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  }
+  }
+
+</style>
+<div class="content">
+
+  <div class="image-wrapper">
+    <img src={reviewData.photo} alt="photo">
+  </div>
+  <div class="image-frame"></div>
+  <div class="review">
+
+    <p>{reviewData.reviewContent}</p>
+
+    <h5>{reviewData.author}</h5>
+    <p>{reviewData.authorPosition}</p>
+
+  </div>
+</div>
