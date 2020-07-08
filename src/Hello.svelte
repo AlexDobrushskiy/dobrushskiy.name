3ef47b7dab24b787026f32901a20b6de8a7d5e7e
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-07-08 16:11:14 +0300

    Scaling

--- src/Hello.svelte
+++ src/Hello.svelte
@@ -3,32 +3,51 @@
   import BgImage from '../assets/bg.jpg';
 </script>
 <style lang="scss">
-  $image-width: 462px;
+  @mixin image($image-width) {
+    .image-frame {
+      margin-left: calc(27px - #{$image-width});
+      position: relative;
+      border-radius: 100px;
+      min-width: $image-width;
+      width: $image-width;
+      height: $image-width;
+      border: solid 1px var(--light-navy);
+      /*z-index: 2;*/
+    }
+
+    .image-wrapper {
+      margin-left: 50px;
+      position: relative;
+      width: $image-width;
+      height: $image-width;
+
+      img {
+        width: $image-width;
+        height: $image-width;
+
+      }
+
+      top: 26px;
+    }
+  }
+
+  @media (min-width: 1280px) {
+    @include image($image-width: 462px);
+  }
+
+  @media (min-width: 992px) and (max-width: 1279px) {
+    @include image($image-width: 358px);
+  }
+
+  @media (max-width: 991px) {
+    @include image($image-width: 277px);
+  }
 
   .main {
     display: flex;
     /*width: calc(100% - 77px);*/
     /*padding-left: 77px;*/
     /*position: relative;*/
-  }
-
-  .image-frame {
-    margin-left: calc(27px - #{$image-width});
-    position: relative;
-    border-radius: 100px;
-    min-width: 462px;
-    width: $image-width;
-    height: $image-width;
-    border: solid 1px var(--light-navy);
-    /*z-index: 2;*/
-  }
-
-  .image-wrapper {
-    margin-left: 50px;
-    position: relative;
-    width: $image-width;
-    height: $image-width;
-    top: 26px;
   }
 
   img {
@@ -44,30 +63,61 @@
     height: 685px;
     background-size: 100%;
     background-repeat: no-repeat;
+
     h1, h3 {
       margin: 0 20px 0 0;
       font-weight: bold;
       display: flex;
       justify-content: flex-end;
     }
+
     h3 {
-      font-size: 24px;
+      @media (min-width: 1280px) {
+        font-size: 24px;
+      }
+
+      @media (min-width: 992px) and (max-width: 1279px) {
+        font-size: 20px;
+      }
+
+      @media (max-width: 991px) {
+        font-size: 17px;
+      }
       font-style: italic;
       /*margin-top: 58px;*/
     }
+
     h1 {
-      font-size: 62px;
+      @media (min-width: 1280px) {
+        font-size: 62px;
+      }
+
+      @media (min-width: 992px) and (max-width: 1279px) {
+        font-size: 46px;
+      }
+
+      @media (max-width: 991px) {
+        font-size: 37px;
+      }
+
     }
+
     p {
       color: var(--black);
       font-size: 18px;
+      @media (max-width: 991px) {
+        font-size: 15px;
+      }
+
       font-weight: 300;
       margin-top: 20px;
+
       a {
         text-decoration: none;
         font-weight: 500;
         color: inherit;
       }
+
       b {
         font-weight: 500;
         font-style: italic;
@@ -83,8 +133,10 @@
   <div class="content-block" style="background-image: url({BgImage})">
     <h1>Alex Dobrushskiy</h1>
     <h3>Software developer</h3>
-    <p>Alex is expirienced independent full stack software developer, who started his professional career in <b>2005</b> and has a great experience of working in small startups and big enterprises.<br/>
+    <p>Alex is experienced independent full stack software developer, who started his professional career in <b>2005</b>
+      and has a great experience of working in small startups and big enterprises.<br/>
       Today his primary professional focus is full stack web development with Python and React.
-      <br/>Alex is a member of <a href="https://www.toptal.com/resume/alex-dobrushskiy#unite-only-masterly-engineers">Toptal network</a>, containing top 3% of freelance talent.
+      <br/>Alex is a member of <a href="https://www.toptal.com/resume/alex-dobrushskiy#unite-only-masterly-engineers">Toptal
+        network</a>, containing top 3% of freelance talent.
   </div>
 </div>
--- src/Recommendations/Slide.svelte
+++ src/Recommendations/Slide.svelte
@@ -4,65 +4,81 @@
   import { faLinkedin } from '@fortawesome/free-brands-svg-icons/faLinkedin';
 </script>
 <style lang="scss">
-  $image-width: 300px;
-  
-  .content {
-    display: flex;
 
-  .image-wrapper {
-    margin-top: 50px;
-    margin-left: 68px;
-    /*position: relative;*/
-    width: $image-width;
-    height: $image-width;
+  @mixin image($image-width) {
+    .image-wrapper {
+      margin-top: 50px;
+      margin-left: 68px;
+      /*position: relative;*/
+      width: $image-width;
+      height: $image-width;
 
-  img {
-    border-radius: 100px;
-    width: $image-width;
-    height: $image-width;
-  }
-
-  }
-
-  .image-frame {
-    margin-top: 33px;
-    margin-left: calc(17px - #{$image-width});
-    border-radius: 100px;
-    min-width: $image-width;
-    width: $image-width;
-    height: $image-width;
-    border: solid 1px var(--light-navy);
-  }
-
-  .review {
-    margin-top: 136px;
-    margin-left: 56px;
-    max-width: 530px;
-
-  p {
-    font-size: 18px;
-    font-weight: 300;
-    color: var(--black);
-    font-style: italic;
-  }
-
-  h5 {
-    margin-top: 40px;
-    margin-bottom: 14px;
-    color: var(--dark-grey);
-    font-size: 24px;
-    font-weight: bold;
-    a {
-      text-decoration: none;
-      color: inherit;
-      span {
-        margin-left: 15px;
-        color: var(--line-blue)
+      img {
+        border-radius: calc(#{$image-width}/4);
+        width: $image-width;
+        height: $image-width;
       }
+
+    }
+
+    .image-frame {
+      margin-top: 33px;
+      margin-left: calc(17px - #{$image-width});
+      border-radius: calc(#{$image-width}/4);
+      min-width: $image-width;
+      width: $image-width;
+      height: $image-width;
+      border: solid 1px var(--light-navy);
     }
   }
 
+  @media (min-width: 1280px) {
+    @include image($image-width: 300px);
   }
+
+  @media (min-width: 992px) and (max-width: 1279px) {
+    @include image($image-width: 225px);
+  }
+
+  @media (max-width: 991px) {
+    @include image($image-width: 180px);
+  }
+
+  .content {
+    display: flex;
+
+
+    .review {
+      margin-top: 136px;
+      margin-left: 56px;
+      max-width: 530px;
+
+      p {
+        font-size: 18px;
+        font-weight: 300;
+        color: var(--black);
+        font-style: italic;
+      }
+
+      h5 {
+        margin-top: 40px;
+        margin-bottom: 14px;
+        color: var(--dark-grey);
+        font-size: 24px;
+        font-weight: bold;
+
+        a {
+          text-decoration: none;
+          color: inherit;
+
+          span {
+            margin-left: 15px;
+            color: var(--line-blue)
+          }
+        }
+      }
+
+    }
   }
 
 </style>
@@ -78,7 +94,7 @@
 
     <h5>
       <a href={reviewData.authorLinkedin}> {reviewData.author} <span><Icon icon={faLinkedin}/></span></a>
-      </h5>
+    </h5>
     <p>{reviewData.authorPosition}</p>
 
   </div>
